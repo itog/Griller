69af7b28fd4311b5af16eaffe661b6415e53e1ed
itog <itog@pigmal.com>
2014-07-22 09:39:02 +0900

    First commit

---
+++ Classes/ViewController.h
@@ -0,0 +1,22 @@
+//
+//  ViewController.h
+//  PwmDrive
+//
+//  Created on 12/26/12.
+//  Copyright (c) 2012 Yukai Engineering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *statusMessage;
+@property (weak, nonatomic) IBOutlet UISlider *firePowerSlider;
+@property (weak, nonatomic) IBOutlet UISlider *rotationSpeedSlider;
+
+- (IBAction)find:(id)sender;
+- (IBAction)changeLedBlightness20:(id)sender;
+- (IBAction)changeLedBlightness50:(id)sender;
+- (IBAction)changeLedBlightness80:(id)sender;
+- (IBAction)changeFirePowerBar:(id)sender;
+@end
